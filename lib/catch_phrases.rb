c74e0b5803a02ff57ec1973042bbdb504d32f3cc
Guligena Aierken <guligena_aierken@163.com>
2019-10-16 03:41:12 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,2 +1,17 @@
-def Mario
-  phrase 
+def mario
+  phrase="It's-a me, Mario!\n"
+  puts phrase
+end
+
+def toadstool
+  status="Thank You Mario! But Our Princess Is In Another Castle!\n"
+  puts status
+end
+
+def link
+  puts "It's Dangerous To Go Alone! Take This.\n"
+end
+
+def any_phrase(phrase)
+  puts phrase
+end 
